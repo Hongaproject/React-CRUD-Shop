f836cfc956ad77ab10ee8ca6c5149f04112fc0ca
Hongaproject <tkwlscjq99@gmail.com>
2023-10-20 12:41:47 +0900

    redux 설치 및 파일 설정

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './app/store';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>
+
+  <Provider store={store}>
+    {/* <React.StrictMode> */}
+      <Router>
+        <App />
+      </Router>
+    {/* </React.StrictMode> */}
+  </Provider>
 );
 
