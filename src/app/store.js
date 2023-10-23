21abbfe44a498a5c796ff095d01c2543311cbce2
Hongaproject <tkwlscjq99@gmail.com>
2023-10-23 14:02:28 +0900

    redux 기본 틀 잡는중

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,22 @@
 import { configureStore, createSlice } from '@reduxjs/toolkit'
 
 export default configureStore({
-  reducer: { }
+  reducer: { 
+    product: product.reducer
+  }
 }) 
+
+let product = createSlice({ // data부분에 있는 부분을 가져와서 사용함.
+  name : 'product',
+  initialState : [{
+    id : 1,
+    title : '오둥이',
+    count : 1
+    },
+    {
+    id : 2,
+    title : '카카오 가습기',
+    count : 1
+    }
+  ]
+})
