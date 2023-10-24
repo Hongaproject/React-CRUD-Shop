d4e22edc23ea9d6b8863c08ea6281d09d8131075
Hongaproject <tkwlscjq99@gmail.com>
2023-10-24 19:04:35 +0900

    createSlice부분 reducers테스트 중

--- src/Test/Basket.jsx
+++ src/Test/Basket.jsx
@@ -1,15 +1,21 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { changeCount } from "../app/store";
 
 function Basket () {
 
-    let basket = useSelector((state)=> state) 
+    let basket = useSelector((state)=> state); 
     // initialState로 지정해준 state들을 꺼내온다.
     // reducer설정을 product: 로 해줘서 product를 가져와야지 내용이 나온다.
 
+    // basket.product는 store.js에 있는 products부분 내용을 가져온다.
+
     console.log(basket);
+
+    let dispatch = useDispatch();
     
     return(
         <div>
+            <h3>{basket.user.name}</h3>
             <table>
                 <thead>
                     <tr>
@@ -19,7 +25,7 @@ function Basket () {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
+                    {/* <tr>
                         <td>{basket.product[0].id}</td>
                         <td>{basket.product[0].title}</td>
                         <td>{basket.product[0].count}</td>
@@ -28,7 +34,20 @@ function Basket () {
                         <td>{basket.product[1].id}</td>
                         <td>{basket.product[1].title}</td>
                         <td>{basket.product[1].count}</td>
-                    </tr>
+                    </tr> */}
+                    {
+                        basket.cart.map((item, index) => (
+                            <tr key={index}>
+                                <td>{item.id}</td>
+                                <td>{item.title}</td>
+                                <td>{item.count}</td>
+                                <button onClick={() => {
+                                    dispatch(changeCount(item.id))
+                                }}>+</button>
+                                
+                            </tr>
+                        ))
+                    }
                 </tbody>
             </table>
         </div>
--- src/app/store.js
+++ src/app/store.js
@@ -2,23 +2,65 @@ import { configureStore, createSlice } from '@reduxjs/toolkit'
 
 // redux를 사용해서 state를 전부 보관함 
 
-let products = createSlice({ // data부분에 있는 부분을 가져와서 사용함.
-  name : 'product',
-  initialState : [{ // 초기 state 할당
-    id : 0,
-    title : '오둥이',
-    count : 1
+// let products = createSlice({ // data부분에 있는 부분을 가져와서 사용함.
+//   name : 'product',
+//   initialState : [{ // 초기 state 할당
+//     id : 0,
+//     title : '오둥이',
+//     count : 1
+//     },
+//     {
+//     id : 1,
+//     title : '카카오 가습기',
+//     count : 1
+//     }
+//   ]
+// })
+
+// let product = createSlice({ // action type, action 생성 함수, reducer까지 한번에 작성이 가능하다.
+//   name : 'product', // reducer 이름을 지정해줌
+//   initialState : [ // 초기상태이며 state 할당
+//   ], // initialState에 있는 내용을 함수와 state와 action을 사용합니다.
+//   reducers: {
+//     addCount(state, action) {
+//       let nums = state.findIndex( a => a.id === action.payload);
+//       state[nums].count++;
+//     },
+//     minusCount(state, action) {
+//       let nums = state.findIndex( a => a.id === action.payload);
+//       if (state[nums].count > 1)  state[nums].count--;
+//     },
+//     deleteCount(state, action) {
+//       action.payload.remove();
+//     },
+//   },
+// });
+
+const user = createSlice({
+  name: "user",
+  initialState: { name: "kim", age: 21 },
+});
+
+const cart = createSlice({
+  name: "cart",
+  initialState: [
+    { id: 0, name: "White and Black", count: 2 },
+    { id: 2, name: "Grey Yordan", count: 1 },
+  ],
+  reducers: {
+    changeCount(state, action) {
+      const i = state.findIndex((product) => product.id === action.payload);
+      state[i].count += 1;
     },
-    {
-    id : 1,
-    title : '카카오 가습기',
-    count : 1
-    }
-  ]
-})
+  },
+});
+
+export const { changeCount } = cart.actions;
 
 export default configureStore({
   reducer: { 
-    product: products.reducer // product부분이 사용하는 파일 부분서 불러지는 부분
+    // product: product.reducer // product부분이 사용하는 파일 부분서 불러지는 부분
+    user: user.reducer,
+    cart: cart.reducer,
   }
 }) 
